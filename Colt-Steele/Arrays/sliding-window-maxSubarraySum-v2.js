8de453431fa802e2a4408fdcb736a2c76acda769
Phillip Sanchez <plang.psm@gmail.com>
2023-02-06 11:59:45 -0800

    sliding windows

---
+++ Colt-Steele/Arrays/sliding-window-maxSubarraySum-v2.js
@@ -0,0 +1,35 @@
+// Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+// Nested loops
+
+const maxSubarraySum = (arr, num) => {
+  // set max = 0
+  // loop through the snap
+  // set the max to sum of the first snap
+  // temp = max
+  // loop through the whole array at position of the num
+  // set temp to temp - the fist number in the previous snap and add the first number after the snap.   [-1, 2, +3, 4]
+  // if temp > max set as max
+
+  // Time complexity O(n) + Space complexity O(n)
+
+  let max = 0;
+  if (num > arr.length) return null;
+
+  for (let i = 0; i < num; i++) {
+    max += arr[i];
+  }
+  let temp = max;
+  for (let i = num; i < arr.length; i++) {
+    temp = temp - arr[i - num] + arr[i];
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+};
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); // 13
+console.log(maxSubarraySum([], 4)); // null
---
+++ Colt-Steele/Arrays/sliding-window-maxSubarraySum.js
@@ -0,0 +1,31 @@
+// Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+// Nested loops
+
+const maxSubarraySum = (arr, num) => {
+  // set max = to lowest possible #
+  // loop through the whole array - length of snap
+  // loop through the snap and grab the sum [i + j]
+  // temp = new snap
+  // if temp > max set max
+
+  // Time complexity O(n^2) + Space complexity O(n)
+
+  if (num > arr.length) return null;
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    let temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+      if (max < temp) {
+        max = temp;
+      }
+    }
+  }
+  return max;
+};
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); // 13
+console.log(maxSubarraySum([], 4)); // null
