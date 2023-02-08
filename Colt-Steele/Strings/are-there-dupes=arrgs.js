914ca9f06b6139e1e4d97ee2c537b0a15afdb784
Phillip Sanchez <plang.psm@gmail.com>
2023-02-08 11:01:42 -0800

    are there dupes solution

--- Colt-Steele/Strings/are-there-dupes=arrgs.js
+++ Colt-Steele/Strings/are-there-dupes=arrgs.js
@@ -1,7 +1,5 @@
 // Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
 
-
-
 // Time complexity O(n)
 
 const areThereDuplicates = (...args) => {
