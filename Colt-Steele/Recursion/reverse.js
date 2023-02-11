5d548405f25556a2c10bd80bf9f48a8e49dd3b7f
Phillip Sanchez <plang.psm@gmail.com>
2023-02-11 12:33:22 -0800

    reverse

---
+++ Colt-Steele/Recursion/reverse.js
@@ -0,0 +1,12 @@
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+function reverse(str) {
+    if(str.length === 1) return str[0]
+    return str[str.length - 1] + reverse(str.substring(0, str.length - 1))
+  }
+  
+  console.log(
+    reverse('awesome'), // 'emosewa'
+    reverse('rithmschool') // 'loohcsmhtir'
+  );
+  
