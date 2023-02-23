9504c1e7ad101ff8e5538c7b6137f999909f8523
Phillip Sanchez <plang.psm@gmail.com>
2023-02-22 16:14:11 -0800

    two sum

---
+++ Colt-Steele/Arrays/two-sum.js
@@ -0,0 +1,20 @@
+var twoSum = function(nums, target) {
+  // val:idx
+    let map = {} // {'1':0, '2':1, }
+    for(let i = 0; i<nums.length; i++) {
+      // define num1 and num2
+      let num1 = nums[i] // 3
+      let num2 = target - num1 // 2
+
+      // check to see if map[num2] is in the hash map
+      if(map[num2] !== undefined) {
+        // if found, return value(index), current index
+        return [map[num2] , i]
+      } else {
+        // add to hash map 'val':idx
+        map[num1] = i
+      }
+    }
+};
+
+console.log(twoSum([1,2,3],5))
