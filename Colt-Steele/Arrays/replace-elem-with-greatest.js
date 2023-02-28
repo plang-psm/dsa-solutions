dbd59fff6475b652750343cc68fba9e36b65d132
Phillip Sanchez <plang.psm@gmail.com>
2023-02-27 16:10:08 -0800

    replace elements with greatest element on the right side

---
+++ Colt-Steele/Arrays/replace-elem-with-greatest.js
@@ -0,0 +1,15 @@
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+// After doing so, return the array.
+
+// Time O(N) | Space O(1)
+var replaceElements = function (arr, max = -1) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const num = arr[i];
+    console.log(num);
+
+    arr[i] = max;
+    max = Math.max(max, num);
+  }
+  return arr;
+};
