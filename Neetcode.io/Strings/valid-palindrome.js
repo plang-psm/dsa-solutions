b1d185abed86f3bd5e3bf9188c62123ca52cb7d2
Phillip Sanchez <plang.psm@gmail.com>
2023-03-10 16:10:55 -0800

    valid palindrome

---
+++ Neetcode.io/Strings/valid-palindrome.js
@@ -0,0 +1,42 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+//-----------------------------
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+// Example 3:
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+
+//-----------------------------
+// format s
+//  lowercase - remove any spaces - join - remove special characters
+// if s === s.reverse() ? T or F
+
+//-----------------------------
+// Time complexity: + Space complexity: 
+
+const isPalindrome = function(s) {
+
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi, '')
+    const re = s.split('').reverse().join('')
+    if(s === re) {
+        return true
+    } else {
+        return false
+    }
+};
+
+console.log(isPalindrome('A man, a plan, a canal: Panama')) // true
