c7d789a98ab8a0a8b073a605a3c60499740f8869
Phillip Sanchez <plang.psm@gmail.com>
2023-03-15 18:12:36 -0700

    3sum

---
+++ Neetcode.io/Two Pointer/3sum.js
@@ -0,0 +1,63 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+ 
+//-----------------------------
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation: 
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+// Example 2:
+
+// Input: nums = [0,1,1]
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+// Example 3:
+
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+
+//-----------------------------
+
+
+//-----------------------------
+// Time complexity: + Space complexity: 
+
+const threeSum = function(nums) {
+    const res = [];
+    nums.sort((a,b) => a-b)
+
+    for (let i = 0; i < nums.length; i++) {
+        const search = nums[i];
+        if (search > 0) break;
+        if (i > 0 && search === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            const threeSum = search + nums[left] + nums[right];
+            if (threeSum > 0) {
+                right--;
+            } else if (threeSum < 0) {
+                left++;
+            } else {
+                res.push([search, nums[left], nums[right]]);
+                left++;
+                right--;
+                while (nums[left] === nums[left - 1] && left < right) {
+                    left++;
+                }
+            }
+        }
+    }
+    return res;
+}
+
+console.log(threeSum([-1,0,1,2,-1,-4])) // [[-1,-1,2],[-1,0,1]]
