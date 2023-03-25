c00e385e0d0dd9791863d93a8205d4c2598860a6
Phillip Sanchez <plang.psm@gmail.com>
2023-03-25 11:37:04 -0700

    concatenation of array

---
+++ Neetcode.io/Arrays/concatenation-of-array.js
@@ -0,0 +1,40 @@
+// Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+// Specifically, ans is the concatenation of two nums arrays.
+
+// Return the array ans.
+
+//-----------------------------
+// Example 1:
+
+// Input: nums = [1,2,1]
+// Output: [1,2,1,1,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+// - ans = [1,2,1,1,2,1]
+// Example 2:
+
+// Input: nums = [1,3,2,1]
+// Output: [1,3,2,1,1,3,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+// - ans = [1,3,2,1,1,3,2,1]
+
+//-----------------------------
+    // arr -> arr + arr 
+    // arr spread
+    // looping twice - O(n)
+        // push the num 
+
+//-----------------------------
+// Time complexity: O(n) + Space complexity: O(1)
+
+const getConcatenation = function (nums) {
+    let arr = [...nums]
+    for (let num of nums) {
+        arr.push(num)
+    }
+    return arr
+};
+  
+  console.log(getConcatenation([1,2,1])) // [1,2,1,1,2,1]
