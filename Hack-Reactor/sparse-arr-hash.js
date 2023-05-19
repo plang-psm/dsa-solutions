181954868865e9440a094002966d1b5af96f29b4
Phillip Sanchez <plang.psm@gmail.com>
2023-05-18 17:45:38 -0700

    sparse arr with hash

---
+++ Hack-Reactor/sparse-arr-hash.js
@@ -0,0 +1,21 @@
+// There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+
+// Pseudocode:
+// while loop
+// first
+// second
+// until we finish the loop
+// return in arr
+
+// Time Complexity: O(n) + Space complexity O(n)
+
+function matchingStrings(strings, queries) {
+    let map = {}
+
+    for (let ch of strings) {
+        map[ch] = (map[ch] || 0) + 1
+    }
+
+    return queries.map(query => map[query] || 0)
+  }
+  
